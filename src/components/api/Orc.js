925add3f0148358b6a2068b0f87ee18977540471
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-05 17:42:10 -0300

    Added API connection

---
+++ src/components/api/Orc.js
@@ -0,0 +1,22 @@
+import API from '.';
+
+export default class Orc {
+
+    constructor({ id, name, thumbnail, age, weight, height, hair_color, professions, friends }) {
+        this.id = id;
+        this.name = name;
+        this.thumbnail = thumbnail;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.hair_color = hair_color;
+        this.professions = professions;
+        this.friends = friends;
+    }
+
+    static async getAll() {
+        const api = await API();
+        const { data } = await api.get('');
+        return data.Brastlewark.map((d) => new Orc(d));
+    }
+}
