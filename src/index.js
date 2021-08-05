ed59de4fb4c093950c05ccbc3a9fa995ec2f9385
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-05 17:44:04 -0300

    Updated index page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Header from '../src/components/header/Header';
+import Footer from '../src/components/footer/Footer';
+import Searchtable from './components/searchTable/SearchTable';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    {/* <App /> */}
+    <Searchtable/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
