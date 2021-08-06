2a6e7445d91958d78c235ac65f6dbbafa55f6626
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-06 00:24:12 -0300

    Spaces changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-import { render, screen,act, findByTestId } from '@testing-library/react';
+import { render, screen, act, findByTestId } from '@testing-library/react';
 import Searchtable from '../src/components/searchTable/SearchTable';
 import Header from '../src/components/header/Header';
 import Footer from '../src/components/footer/Footer';
 test('render header', () => {
- const component = render(<Header/>)
- component.getByText('Heroes of the Earth');
+  const component = render(<Header />)
+  component.getByText('Heroes of the Earth');
 });
 test('footer test', () => {
-  const component = render(<Footer/>)
+  const component = render(<Footer />)
   component.getByText('© 2021 - Federico Speranza - Heroes Corp');
- });
+});
