d550a984462f0fa4552945cf2ceac84909561fc7
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-06 00:17:48 -0300

    Added tests for header and footer

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen,act, findByTestId } from '@testing-library/react';
+import Searchtable from '../src/components/searchTable/SearchTable';
+import Header from '../src/components/header/Header';
+import Footer from '../src/components/footer/Footer';
+test('render header', () => {
+ const component = render(<Header/>)
+ component.getByText('Heroes of the Earth');
 });
+test('footer test', () => {
+  const component = render(<Footer/>)
+  component.getByText('© 2021 - Federico Speranza - Heroes Corp');
+ });
